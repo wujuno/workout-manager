8bde0ffba76889d68ed9ef94474c2a80a72f5769
wujuno <huj88854@gmail.com>
2023-02-02 23:58:29 +0900

    delete import

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Root from "./Root";
 import Home from "./screens/Home";
@@ -7,50 +7,48 @@ import Record from "./screens/Record";
 import SignUp from "./screens/Signup";
 import Watch from "./screens/watch/Watch";
 import Date from "./screens/watch/Date";
-import Test from "./screens/Test";
 import SeeOneRecord from "./screens/watch/SeeOneRecord";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root/>,
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "record",
+        element: <Record />,
+      },
+      {
+        path: "watch/:username",
+        element: <Watch />,
         children: [
-            {
-                path: "",
-                element: <Home/>
-            },
-            {
-                path: "record",
-                element: <Record/>,
-            },
-            {
-                path: "watch/:username",
-                element: <Watch/>,
-                children: [
-                    {
-                        path:"sdate",
-                        element: <Date/>,
-                        children: [
-                            {
-                                path:":date",
-                                element: <SeeOneRecord/>
-                            }
-                        ]
-                    }
-                ]
-                
-            },
-            {
-                path: "login",
-                element: <LogIn/>
-            },
-            {
-                path: "signup",
-                element: <SignUp/>
-            },
+          {
+            path: "sdate",
+            element: <Date />,
+            children: [
+              {
+                path: ":date",
+                element: <SeeOneRecord />,
+              },
+            ],
+          },
         ],
-        errorElement: <NotFound/>
-    }
-])
+      },
+      {
+        path: "login",
+        element: <LogIn />,
+      },
+      {
+        path: "signup",
+        element: <SignUp />,
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
 
 export default router;
