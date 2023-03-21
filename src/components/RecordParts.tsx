301744508a1d4c2cd9241aa13df021e82fc973e6
wujuno <huj88854@gmail.com>
2023-03-21 12:29:08 +0900

    Feat: 하체 운동종목 추가

--- src/components/RecordParts.tsx
+++ src/components/RecordParts.tsx
@@ -1,55 +1,56 @@
-
 export const bigPartsArr = [
-    {name: "가슴", id:1},
-    {name: "등", id:2},
-    {name: "어깨", id:3},
-    {name: "하체", id:4},
-    {name: "복근", id:5},
-]
+  { name: "가슴", id: 1 },
+  { name: "등", id: 2 },
+  { name: "어깨", id: 3 },
+  { name: "하체", id: 4 },
+  { name: "복근", id: 5 },
+];
 
 export const Chest = [
-    {id:1, name: "플랫 벤치프레스"},
-    {id:2, name: "인클라인 벤치프레스"},
-    {id:3, name: "딥스"},
-    {id:4, name: "케이블 플라이"},
-    {id:5, name: "머신 플라이"},
-    {id:6, name: "플랫 벤치 덤벨프레스"},
-    {id:7, name: "인클라인 벤치 덤벨프레스"},
-]
+  { id: 1, name: "플랫 벤치프레스" },
+  { id: 2, name: "인클라인 벤치프레스" },
+  { id: 3, name: "딥스" },
+  { id: 4, name: "케이블 플라이" },
+  { id: 5, name: "머신 플라이" },
+  { id: 6, name: "플랫 벤치 덤벨프레스" },
+  { id: 7, name: "인클라인 벤치 덤벨프레스" },
+];
 
 export const Back = [
-    {id:1, name: "랫 풀 다운"},
-    {id:2, name: "턱걸이"},
-    {id:3, name: "컨벤셔널 데드리프트"},
-    {id:4, name: "시티드 케이블 로우"},
-    {id:5, name: "머신 로우"},
-    {id:6, name: "원 암 덤벨 로우"},
-    {id:7, name: "케이블 풀 오버"},
-]
+  { id: 1, name: "랫 풀 다운" },
+  { id: 2, name: "턱걸이" },
+  { id: 3, name: "컨벤셔널 데드리프트" },
+  { id: 4, name: "시티드 케이블 로우" },
+  { id: 5, name: "머신 로우" },
+  { id: 6, name: "원 암 덤벨 로우" },
+  { id: 7, name: "케이블 풀 오버" },
+];
 
 export const Shoulder = [
-    {id:1, name: "사이드 레터럴 레이즈"},
-    {id:2, name: "바벨 숄더 프레스"},
-    {id:3, name: "숄더 프레스 머신"},
-    {id:4, name: "페이스 풀"},
-    {id:5, name: "리버스 펙 덱 플라이"},
-]
+  { id: 1, name: "사이드 레터럴 레이즈" },
+  { id: 2, name: "바벨 숄더 프레스" },
+  { id: 3, name: "숄더 프레스 머신" },
+  { id: 4, name: "페이스 풀" },
+  { id: 5, name: "리버스 펙 덱 플라이" },
+];
 
 export const Leg = [
-    {id:1, name: "스쿼트"},
-    {id:2, name: "덤벨 런지"},
-    {id:3, name: "레그 프레스 머신"},
-]
+  { id: 1, name: "스쿼트" },
+  { id: 2, name: "덤벨 런지" },
+  { id: 3, name: "레그 프레스 머신" },
+  { id: 4, name: "레그 컬" },
+  { id: 5, name: "레그 익스텐션" },
+];
 
 export const Abs = [
-    {id:1, name: "크런치"},
-    {id:2, name: "레그레이즈"},
-    {id:3, name: "행잉 레그레이즈"},
-]
+  { id: 1, name: "크런치" },
+  { id: 2, name: "레그레이즈" },
+  { id: 3, name: "행잉 레그레이즈" },
+];
 
 export const countTitleArr = [
-    {id:1, name:"setTimes"},
-    {id:2, name:"times"},
-    {id:3, name:"weight"},
-    {id:4, name:"restTime"}
-]
+  { id: 1, name: "setTimes" },
+  { id: 2, name: "times" },
+  { id: 3, name: "weight" },
+  { id: 4, name: "restTime" },
+];
